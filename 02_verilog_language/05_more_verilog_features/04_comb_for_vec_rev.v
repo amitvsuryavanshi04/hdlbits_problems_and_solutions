65f3d707b51a67e39ba8f002e1a2a97ed827e0ca
amitvsuryavanshi04 <amitvsuryavanshi04@gmail.com>
2025-08-02 16:23:53 +0530

    'Added solution for more verilog features sublock part  - 03.'

---
+++ 02_verilog_language/05_more_verilog_features/04_comb_for_vec_rev.v
@@ -0,0 +1,15 @@
+//Question: Given a 100-bit input vector [99:0], reverse its bit ordering. 
+
+
+//solution:
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+    integer i;
+    always @ (in) begin 
+        for (i=0;i<100; i=i+1) begin
+            out[99-i] = in[i];
+        end        
+    end
+endmodule
